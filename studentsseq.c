32a6d8c6021a3c0af8a7ebb8be8849605947ac95
Lucas-KC <lucaskimocosta@hotmail.com>
2026-04-22 16:47:11 -0300

    Modifiquei para 3 digitos após a virgula o valor do tempo printado no arquivo sequencial

--- studentsseq.c
+++ studentsseq.c
@@ -383,7 +383,7 @@ int main(int argc, char **argv) {
     print_out_region(out_region, R);
     print_out_country(out_country);
     print_awards(best_region, max_region_avg, best_city_r, best_city_c, max_city_avg);
-    printf("Tempo de resposta em segundos, sem considerar E/S: %.6f s\n", time);
+    printf("Tempo de resposta em segundos, sem considerar E/S: %.3f s\n", time);
     
     // DEBUG PRINT
     // print_average_student(average_to_city, R, C, A);
